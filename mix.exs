8ac82c6df0b207938e2dda8c700a73e32a8886fa
Lain Soykaf <lain@lain.com>
2023-11-23 19:44:30 +0400

    More packaging fixes.

--- mix.exs
+++ mix.exs
@@ -7,10 +7,13 @@ defmodule SimpleEmbedder.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      source_url: "https://github.com/lambadalambda/simple_embedder",
-      name: "SimpleEmbedder",
-      licenses: ["CC0-1.0"],
-      links: [],
+      package: %{
+        description: "A package that wraps popular python-based embedding generators.",
+        links: [],
+        source_url: "https://github.com/lambadalambda/simple_embedder",
+        name: "SimpleEmbedder",
+        licenses: ["CC0-1.0"]
+      },
       deps: deps()
     ]
   end
@@ -26,7 +29,8 @@ defmodule SimpleEmbedder.MixProject do
   defp deps do
     [
       {:erlport, "~> 0.11.0"},
-      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:mix_test_watch, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
